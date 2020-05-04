7f1327d1f4e64c3de3531d664eecac448fc0d88f
shohei morita <shomorita1985@gmail.com>
2020-05-04 17:10:25 +0900

    propertiesテーブルを作成

--- db/migrate/20200504074734_create_properties.rb
+++ db/migrate/20200504074734_create_properties.rb
@@ -1,7 +1,11 @@
 class CreateProperties < ActiveRecord::Migration[5.2]
   def change
     create_table :properties do |t|
-
+      t.string :name
+      t.integer :string
+      t.string  :address
+      t.integer :age
+      t.text :note
       t.timestamps
     end
   end
