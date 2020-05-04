a33c950501bf9adde1db647f8878a69f8f41f321
shohei morita <shomorita1985@gmail.com>
2020-05-04 19:35:52 +0900

    物件登録機能実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,11 +3,29 @@ class PropertiesController < ApplicationController
   end
 
   def new
+    @property = Property.new
+    @property.nearest_stations.build
+  end
+
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      redirect_to properties_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :cost, :address, :age, :note,
+                                     nearest_station_attributes: %i(line station minute_walk))
   end
 end
