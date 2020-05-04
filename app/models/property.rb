c1fb032207a59c03cb380ac56d3861b1a8bd326c
shohei morita <shomorita1985@gmail.com>
2020-05-04 18:01:01 +0900

    propertiesコントローラーとその付帯viewの作成

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
   has_many :neartest_staitons
-  accepts_nested_attributes_for :nearest_stations
+  accepts_nested_attributes_for :nearest_stations, allow_destroy: true
 end
